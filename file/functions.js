52e7399843ce4c53227fc43418f5e36d193ed743
Aswin <guruaswin72@gmail.com>
2025-09-09 20:21:46 +0530

    Update functions.js
    changed time to include year

--- file/functions.js
+++ file/functions.js
@@ -35,28 +35,40 @@ $(window).resize(function() {
 	};
 })(jQuery);
 
-function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">" + minutes; 
-	$("#clock").html(result);
+function timeElapse(date) {
+    var current = new Date();
+    var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 
-	var text = "THE WORLD JUST GOT LUCKIER SINCE ";
-	$("#message-box").html(text);
+    // calculate years
+    var years = Math.floor(seconds / (3600 * 24 * 365));
+    seconds = seconds % (3600 * 24 * 365);
 
+    // calculate days after years
+    var days = Math.floor(seconds / (3600 * 24));
+    seconds = seconds % (3600 * 24);
+
+    // calculate hours
+    var hours = Math.floor(seconds / 3600);
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+    seconds = seconds % 3600;
+
+    // calculate minutes
+    var minutes = Math.floor(seconds / 60);
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    // ⏱️ final display
+    var result = "Years <span class=\"digit\">" + years + "</span> " +
+                 "Days <span class=\"digit\">" + days + "</span> " +
+                 "Hours <span class=\"digit\">" + hours + "</span> " +
+                 "Minutes <span class=\"digit\">" + minutes + "</span>";
+
+    $("#clock").html(result);
+
+    var text = "THE WORLD JUST GOT LUCKIER SINCE ";
+    $("#message-box").html(text);
 }
+
